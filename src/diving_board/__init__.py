fb319e3e5e73d2c099660efd00b1f062fdcb71bd
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2025-11-26 17:28:29 -0800

    Update to use AbstractGapiClient

--- src/diving_board/__init__.py
+++ src/diving_board/__init__.py
@@ -1,26 +1,31 @@
 import json
 import logging
 import re
+import uuid
+from pathlib import Path
 from typing import Any
 
 import requests
-from gapi import GapiCustomizations
-from pydantic import ValidationError
+from gapi import (
+    AbstractGapiClient,
+    GapiCustomizations,
+    apply_customizations,
+    update_json_schema_and_pydantic_model,
+)
 
-from diving_board.constants import FILES_PATH
+from diving_board.constants import DIVING_BOARD_PATH, FILES_PATH
 from diving_board.exceptions import HTTPError
 from diving_board.other_seasons import OtherSeasonsMixin
 from diving_board.other_seasons.models import OtherSeasons
 from diving_board.season import SeasonMixin
 from diving_board.season.models import Season
-from diving_board.update_files import save_file, update_model
 
 RESPONSE_MODELS = OtherSeasons | Season
 
 default_logger = logging.getLogger(__name__)
 
 
-class DivingBoard(SeasonMixin, OtherSeasonsMixin):
+class DivingBoard(AbstractGapiClient, SeasonMixin, OtherSeasonsMixin):
     def __init__(self, logger: logging.Logger = default_logger) -> None:
         self.logger = logger
         self.api_key = ""
@@ -119,35 +124,25 @@ class DivingBoard(SeasonMixin, OtherSeasonsMixin):
 
         return response.json()
 
-    def _parse_response[T: RESPONSE_MODELS](
+    def save_file(self, name: str, data: dict[str, Any]) -> None:
+        """Add a new test file for a given endpoint."""
+        input_folder = FILES_PATH / name
+        new_json_path = input_folder / f"{uuid.uuid4()}.json"
+        new_json_path.parent.mkdir(parents=True, exist_ok=True)
+        new_json_path.write_text(json.dumps(data, indent=2))
+
+    def update_model(
         self,
-        response_model: type[T],
-        data: dict[str, Any],
         name: str,
         customizations: GapiCustomizations | None = None,
-    ) -> T:
-        try:
-            parsed = response_model.model_validate(data)
-        except ValidationError as e:
-            save_file(name, data)
-            update_model(name, customizations)
-            msg = "Parsing error, model updated, try again."
-            raise ValueError(msg) from e
+    ) -> None:
+        """Update a specific response model based on input data."""
+        schema_path = DIVING_BOARD_PATH / f"{name}/schema.json"
+        model_path = DIVING_BOARD_PATH / f"{name}/models.py"
+        files_path = FILES_PATH / name
+        update_json_schema_and_pydantic_model(files_path, schema_path, model_path, name)
+        apply_customizations(model_path, customizations)
 
-        if self.dump_response(parsed) != data:
-            save_file(name, data)
-            temp_path = FILES_PATH / "_temp"
-            named_temp_path = temp_path / name
-            named_temp_path.mkdir(parents=True, exist_ok=True)
-            original_path = named_temp_path / "original.json"
-            parsed_path = named_temp_path / "parsed.json"
-            original_path.write_text(json.dumps(data, indent=2))
-            parsed_path.write_text(json.dumps(self.dump_response(parsed), indent=2))
-            msg = "Parsed response does not match original response."
-            raise ValueError(msg)
-
-        return parsed
-
-    def dump_response(self, data: RESPONSE_MODELS) -> dict[str, Any]:
-        """Dump an API response to a JSON serializable object."""
-        return data.model_dump(mode="json", by_alias=True, exclude_unset=True)
+    def files_path(self) -> Path:
+        """Get the path to the files directory."""
+        return FILES_PATH
