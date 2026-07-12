b69b4529e53c4bd54745745d8e0844cf57ebea87
ryn-cx <262888154+ryn-cx@users.noreply.github.com>
2026-07-11 23:19:05 -0700

    Remove playlist

--- src/diving_board/playlist/bucket/__init__.py
+++
@@ -1,9 +0,0 @@
-# TODO: Validate
-"""Playlist bucket extractors."""
-
-from diving_board.playlist.bucket.playlist import (
-    PlaylistBucketPlaylist as PlaylistBucketPlaylist,
-)
-from diving_board.playlist.bucket.related import (
-    PlaylistBucketRelated as PlaylistBucketRelated,
-)
